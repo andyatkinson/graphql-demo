6c9b2d815fd2fd49d1bc5f0f0d1c32449a4a8362
Andy Atkinson <andyatkinson@gmail.com>
2017-07-20 12:37:38 -0500

    Passing arguments

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ var { buildSchema } = require('graphql');
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
     type Query {
+      rollDice(numDice: Int!, numSides: Int): [Int]
       hello: String
     }
   `);
 
 // The root provides a resolved function for each API endpoint
 var root = {
+  rollDice: function({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+
   hello: () => {
     return 'Hello World!';
   },
